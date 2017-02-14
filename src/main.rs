c877368161710a484f30c8cbced10b34ae62444e
Joacim Åström <joacim.astrom@gmail.com>
2017-02-14 12:03:36 +0100

    evaluate returns f32 instead of bool

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ extern crate rand;
 use rustc_serialize::json;
 use std::collections::HashMap;
 use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
 
 #[derive(Debug)]
 enum CompareType {
     Equal,
     GreaterThanOrEqual,
     LessThanOrEqual,
-    IntervalMatch,
+    IntervalMatch(u16, u16),
 }
 
 #[derive(Debug)]
@@ -19,8 +20,6 @@ struct Attribute {
     name: String,
     values: Vec<String>,
     compare_type: CompareType,
-    up: u16,
-    down: u16,
 }
 
 impl Attribute {
@@ -36,16 +35,22 @@ impl Attribute {
         }
     }
 
-    fn evaluate_values(&self, a: &u16, b: &u16) -> bool {
+    fn evaluate_values(&self, a: &u16, b: &u16) -> f32 {
         if *a == 0 || *b == 0 {
-            return false;
+            return 0.0;
         }
 
         match self.compare_type {
-            CompareType::Equal => *b == *a,
-            CompareType::GreaterThanOrEqual => *b >= *a,
-            CompareType::LessThanOrEqual => *b <= *a,
-            CompareType::IntervalMatch => *b <= (*a + self.up) && *b >= (if self.down >= *a {0} else  {*a - self.down})
+            CompareType::Equal => if *b == *a { 1.0 } else { 0.0 },
+            CompareType::GreaterThanOrEqual => if *b >= *a { 1.0 } else { 0.0 },
+            CompareType::LessThanOrEqual => if *b <= *a { 1.0 } else { 0.0 },
+            CompareType::IntervalMatch(up, down) => {
+                if *b <= *a + up && *b >= if down >= *a { 0 } else { *a - down } {
+                    1.0
+                } else {
+                    0.0
+                }
+            }
         }
     }
 }
@@ -71,7 +76,6 @@ struct SimpleModel<'a> {
     products: &'a HashMap<u32, Product>,
     matches: &'a Vec<Match>,
     match_count: Vec<u32>,
-    match_sum: u32,
 }
 
 impl<'a> SimpleModel<'a> {
@@ -81,62 +85,57 @@ impl<'a> SimpleModel<'a> {
             let new = self.products.get(&new).unwrap();
 
             for (i, attribute) in self.attributes.iter().enumerate() {
-                if attribute.evaluate_values(&original.values[i], &new.values[i]) {
+                if attribute.evaluate_values(&original.values[i], &new.values[i]) > 0.0 {
                     self.match_count[i] += 1;
-                    self.match_sum += 1;
                 }
             }
         }
     }
 
-    fn score(&self, a: &u32, b: &u32) -> u32 {
+    fn score(&self, a: &u32, b: &u32) -> f32 {
         let original = self.products.get(&a).unwrap();
         let new = self.products.get(&b).unwrap();
-        let mut score = 0;
+        let mut score: f32 = 0.0;
 
         for (i, attribute) in self.attributes.iter().enumerate() {
-            if attribute.evaluate_values(&original.values[i], &new.values[i]) {
-                score += self.match_count[i];
-            }
+            score += self.match_count[i] as f32 *
+                     attribute.evaluate_values(&original.values[i], &new.values[i])
         }
-
         score
     }
 
-    fn find_all_matches(&self, original: &u32) -> Vec<(u32, u32)> {
-        let mut matches: Vec<(u32, u32)> = Vec::new();
+    fn find_all_matches(&self, original: &u32) -> Vec<(f32, u32)> {
+        let mut matches: Vec<(f32, u32)> = Vec::new();
 
         for k in self.products.keys() {
             let score = self.score(original, k);
             matches.push((score, *k));
         }
 
-        matches.sort_by(|&(a, _), &(b, _)| b.cmp(&a));
+        matches.sort_by(|&(a, _), &(b, _)| b.partial_cmp(&a).unwrap_or(Ordering::Equal));
 
         matches
     }
 }
 
 fn main() {
-    let mut rdr =
-        csv::Reader::from_file("./data/attributes.csv").unwrap().has_headers(false);
+    let mut rdr = csv::Reader::from_file("./data/attributes.csv").unwrap().has_headers(false);
     let mut attributes = Vec::new();
     for record in rdr.decode() {
-        let (name, values, compare_type, up, down): (String, String, u16, u16, u16) = record.unwrap();
+        let (name, values, compare_type, up, down): (String, String, u16, u16, u16) =
+            record.unwrap();
         let values: Vec<String> = values.split(',').map(|s| s.to_string()).collect();
         let compare_type = match compare_type {
             1 => CompareType::Equal,
             2 => CompareType::GreaterThanOrEqual,
             3 => CompareType::LessThanOrEqual,
-            6 => CompareType::IntervalMatch,
+            6 => CompareType::IntervalMatch(up, down),
             _ => panic!("Unknown compare type: {}", compare_type),
         };
         attributes.push(Attribute {
             name: name,
             values: values,
             compare_type: compare_type,
-            up: up, 
-            down: down,
         })
     }
 
@@ -185,7 +184,6 @@ fn main() {
         products: &products,
         matches: &train.to_owned(),
         match_count: vec![0; attributes.len()],
-        match_sum: 0,
     };
     simple_model.train();
 
